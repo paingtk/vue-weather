a11815a4f919aa9c2d2a1b6bcf7fecaf41118c6d
Paing Thu Kyaw <paingthukyaw534@gmail.com>
2022-08-30 18:20:27 +0630

    feat: implement mapbox API to search places
    chore: add axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,51 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const mapboxAPIKey =
+  'pk.eyJ1IjoicGFpbmd0aHVreWF3IiwiYSI6ImNsN2cxeWUydjAwOTkzb21uc2ZjcWpidzYifQ.D__sV-k-x66BF5YnNbDBfg'
+const searchQuery = ref('')
+const queryTimeout = ref(null)
+const mapboxSearchResults = ref(null)
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value)
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== '') {
+      const result = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+      )
+      mapboxSearchResults.value = result.data.features
+      console.log(mapboxSearchResults.value)
+      return // need to include this for not to excute the below line if the condition is true!
+    }
+    mapboxSearchResults.value = null
+  }, 300)
+}
+</script>
 
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        v-model="searchQuery"
+        @input="getSearchResults"
+        type="text"
+        placeholder="Search for a city"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        v-if="mapboxSearchResults"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
